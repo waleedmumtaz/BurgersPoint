4bf230aa677411a4ce154df2cfe67fb88f1f9882
waleedmumtaz <waleedmumtaz@outlook.com>
2019-03-21 16:23:33 +0500

    Tested Hamburger class by adding some code in the Main method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Hamburger hamburger = new Hamburger("Basic", "Sausage", 3.89, "White");
 
+//        double price = hamburger.itemizeHamburger();
+
+        hamburger.addHamburgerAdditionOne("Tomato", 0.36);
+        hamburger.addHamburgerAdditionTwo("Carrots", 0.41);
+        hamburger.addHamburgerAdditionThree("Cucumber", 0.25);
+        hamburger.addHamburgerAdditionFour("Olives", 0.40);
+//        price = hamburger.itemizeHamburger();
+        System.out.println("Total burger price is " + hamburger.itemizeHamburger());
     }
 }
